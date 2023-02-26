f040740f93058231f532db733ae2ba2eec1716cf
gracejoo26 <gracejoo26@gmail.com>
2023-02-26 10:42:32 +0900

    vue로 부동산 사이트 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>{{ price1 }}만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ price2 }}만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ price2 }}만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      //data 보관함  object자료로 저장해둠 {자료이름:자료내용}
+      //data 바인딩 하는 이유 
+      //1) html에 하드코딩 해놓으면 나중에 변경 어려움
+      //2) 실시간 렌더링 기능을 이용하려고(웹앱 사용가능 부드럽게 넘어감, 새로고침 하지 않아도 부드럽게 변경 가능)
+      price1:80,
+      price2:70,
+      products:['역삼동원룸','천호동원룸','마포구원룸']
+    }
+  },
+  components:{
+
   }
 }
 </script>
