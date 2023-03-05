528e545640ce40bfd22c798c7dad847a673114ee
gracejoo26 <gracejoo26@gmail.com>
2023-03-05 17:01:46 +0900

    vue click event

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,33 @@
     <a v-for="a in 메뉴들" :key="a">{{ a }}</a>
 <!--변수 작명 2개까지 가능, (a,i) 왼쪽변수는 array내의 데이터, 오른쪽 변수는 1씩 증가하는 정수-->
   </div>
-  <div v-for="(원룸상품,i) in products" :key="i">
-    <h4>{{ products[i] }}</h4>
-    <p >{{ price[i] }}만원</p>
-    <!--두개 다 3번반복=>i를 써서 반복가능-->
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>{{ price1 }}만원</p>
+    <button @click="increase">허위매물신고</button>
+    <span> 신고수: {{ 신고수 }}</span>
+    <!--
+      v-on:Click = @Click
+
+      쌩 js스타일: 
+      1.버튼누르면 숫자 찾아서 +1해줘,
+      2.그리고 +1된걸 HTML에 반영해줘
+
+      vue 스타일
+      실시간 렌더링으로 신고수에+1
+      1.버튼 누르면 관련된 데이터만 +1,
+
+      event명 정리:
+      @mouseover,ctrl+space누르면 나옴
+    -->
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ price2 }}만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ price2 }}만원</p>
   </div>
 </template>
 
@@ -21,11 +44,18 @@ export default {
       //data 바인딩 하는 이유 
       //1) html에 하드코딩 해놓으면 나중에 변경 어려움
       //2) 실시간 렌더링 기능을 이용하려고(웹앱 사용가능 부드럽게 넘어감, 새로고침 하지 않아도 부드럽게 변경 가능)
+      price1:80,
+      price2:70,
       products:['역삼동원룸','천호동원룸','마포구원룸'],
-      price:['80','70','70'],
       메뉴들:['Home','Shop','About'],
+      신고수:0,
     }
   },
+  methods:{
+    increase(){
+      this.신고수 +=1;
+    }
+  },//this는 위에 data를 지칭함. 함수안에 데이터 쓸땐 this.데이터명
   components:{
 
   }
